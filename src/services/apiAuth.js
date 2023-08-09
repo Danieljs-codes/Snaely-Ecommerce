e6c4e77837a358a4eb27e6323a705e0b5f12cacf
Danieljs-codes <olamideonabiyi@student.oauife.edu.ng>
2023-08-09 23:19:32 +0100

    Minor Bug fix

--- src/services/apiAuth.js
+++ src/services/apiAuth.js
@@ -41,6 +41,7 @@ export async function signOut() {
 
 export async function deleteAccount() {
   const { error } = await supabase.rpc('delete_user');
+  signOut();
 
   if (error) throw new Error(error.message || 'Error deleting account');
 }
